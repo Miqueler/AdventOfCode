7835f64bfd3e35cf27e262c247e63f90282fe2e9
Miqueler <miquelparisisaura@icloud.com>
2025-12-15 17:47:43 +0100

    finaly solved/Users/miquelparisisaura/miniconda3/bin/python /Users/miquelparisisaura/Documents/Coding/AdventOfCode/day2/part2.pyturns out that when you use your brain to think stuff works out

--- day2/part2.py
+++ day2/part2.py
@@ -7,6 +7,7 @@ num = str()
 start, end = int(), int()
 current = 0
 total = 0
+flag = 0
 
 for i in range(len(id_list)): #Loops over the different ranges
     #Get the range of values
@@ -18,32 +19,27 @@ for i in range(len(id_list)): #Loops over the different ranges
     
 
     while end >= current: #loop over all numbers in the range
-        flag = 0
-        
         num = str(current)
         #Loop over all the possible groups of "sub numbers" (ex: 1111 -> 1-1-1-1, 11-11)
-        print(num)
         for j in range(1, (len(num) // 2) + 1): #Try for every possible group of convs
             if len(num) % j != 0: 
-                current += 1
                 continue
             #Try current conv
             for i in range((len(num) // j)):
                 
                 if ((i+2)*j) > len(num): break
                 
-                print(f"first: {num[(i * j):((i+1)*j)]} second: {num[((i+1)*j):((i+2)*j)]}")
                 if num[(i * j):((i+1)*j)] != num[((i+1)*j):((i+2)*j)]:
                     flag = 1
-                    print(True)
+                    #print(True)
                     break
-                else:
-                    print(False)
-
+                #else:
+                #    print(False)
             if flag == 0: 
                 total += current
-                continue
-        print(total)
+                break
+            flag = 0
+        #print(total)
         current += 1
 
 print(total)
